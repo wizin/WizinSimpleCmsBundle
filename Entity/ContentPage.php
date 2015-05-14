99545a219376037b72a2951ca60f3c01eb46b733
Makoto Hashiguchi <gusagi@gmail.com>
2015-05-14 19:38:18 +0900

    Add setter and getter method

--- Entity/ContentPage.php
+++ Entity/ContentPage.php
@@ -119,4 +119,188 @@ class ContentPage
     {
         return $this->id;
     }
+
+    /**
+     * Set pathInfo
+     *
+     * @param string $pathInfo
+     * @return ContentPage
+     */
+    public function setPathInfo($pathInfo)
+    {
+        $this->pathInfo = $pathInfo;
+
+        return $this;
+    }
+
+    /**
+     * Get pathInfo
+     *
+     * @return string 
+     */
+    public function getPathInfo()
+    {
+        return $this->pathInfo;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return ContentPage
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return ContentPage
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set template
+     *
+     * @param string $template
+     * @return ContentPage
+     */
+    public function setTemplate($template)
+    {
+        $this->template = $template;
+
+        return $this;
+    }
+
+    /**
+     * Get template
+     *
+     * @return string 
+     */
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+
+    /**
+     * Set beginAt
+     *
+     * @param \DateTime $beginAt
+     * @return ContentPage
+     */
+    public function setBeginAt($beginAt)
+    {
+        $this->beginAt = $beginAt;
+
+        return $this;
+    }
+
+    /**
+     * Get beginAt
+     *
+     * @return \DateTime 
+     */
+    public function getBeginAt()
+    {
+        return $this->beginAt;
+    }
+
+    /**
+     * Set endAt
+     *
+     * @param \DateTime $endAt
+     * @return ContentPage
+     */
+    public function setEndAt($endAt)
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    /**
+     * Get endAt
+     *
+     * @return \DateTime 
+     */
+    public function getEndAt()
+    {
+        return $this->endAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return ContentPage
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return ContentPage
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
 }
