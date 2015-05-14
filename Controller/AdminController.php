17c5a6d9d57094fc79275d01528cd05eca881d55
Makoto Hashiguchi <gusagi@gmail.com>
2015-05-14 20:35:39 +0900

    Generate FrontController and AdminController

---
+++ Controller/AdminController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wizin\Bundle\SimpleCmsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminController extends Controller
+{
+}
---
+++ Tests/Controller/AdminControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wizin\Bundle\SimpleCmsBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminControllerTest extends WebTestCase
+{
+}
