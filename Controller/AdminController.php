1e501e16ed9d0a71564e939711ce9b4c54141cd8
Makoto Hashiguchi <gusagi@gmail.com>
2015-05-17 00:54:14 +0900

    Add blank action WizinSimpleCmsBundle:listAction

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -3,7 +3,24 @@
 namespace Wizin\Bundle\SimpleCmsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * Class AdminController
+ * @package Wizin\Bundle\SimpleCmsBundle\Controller
+ */
 class AdminController extends Controller
 {
+
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function listAction()
+    {
+        return array(
+            // ...
+        );
+    }
 }
