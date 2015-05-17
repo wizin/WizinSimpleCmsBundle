7199c471179ae249f48616fdd0e7cbb33adab0a3
Makoto Hashiguchi <gusagi@gmail.com>
2015-05-17 16:32:16 +0900

    Add some testcases

--- Tests/Service/TemplateTest.php
+++ Tests/Service/TemplateTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace Wizin\Bundle\SimpleCmsBundle\Tests\Service;
 
+use Symfony\Component\HttpFoundation\Request;
 use Wizin\Bundle\BaseBundle\TestCase\ServiceTestCase;
+use Wizin\Bundle\SimpleCmsBundle\Entity\Content;
 
 class TemplateTest extends ServiceTestCase
 {
@@ -51,6 +53,88 @@ class TemplateTest extends ServiceTestCase
     }
 
     /**
+     * @test
+     */
+    public function getTemplateFilePath()
+    {
+        $templateFile = 'default.html.twig';
+        $this->assertEquals($this->getTestFilePath(), $this->getService()->getTemplateFilePath($templateFile));
+    }
+
+    /**
+     * @test
+     * @dataProvider isExistsProvider
+     */
+    public function isExists($templateFile, $expected)
+    {
+        $this->assertSame($expected, $this->getService()->isExists($templateFile));
+    }
+
+    /**
+     * data provider for $this->isExists()
+     *
+     * @return array
+     */
+    public function isExistsProvider()
+    {
+        return [
+            ['default.html.twig', true],
+            ['test.html.twig', false],
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function getPlaceholders()
+    {
+        $this->assertSame(['body'], $this->getService()->getPlaceholders('default.html.twig'));
+    }
+
+    /**
+     * @test
+     * @dataProvider generateResponseContentProvider
+     */
+    public function generateResponseContent(Content $content, $expected)
+    {
+        $this->getContainer()->enterScope('request');
+        $this->getContainer()->set('request', new Request(), 'request');
+        $responseContent = $this->getService()->generateResponseContent($content);
+        $this->assertContains('<title>' .$expected['title'] .'</title>', $responseContent);
+        $this->assertContains($expected['body'], $responseContent);
+    }
+
+    /**
+     * data provider for $this->generateResponseContent()
+     *
+     * @return array
+     */
+    public function generateResponseContentProvider()
+    {
+        $data = [];
+        $title = 'test page';
+        $body = '<h1>Test</h1>';
+        $testContent = (new \Wizin\Bundle\SimpleCmsBundle\Entity\Content())
+            ->setPathInfo('/test')
+            ->setTitle($title)
+            ->setParameters(['body' => $body])
+            ->setTemplate('default.html.twig')
+        ;
+        $data[] = [$testContent, ['title' => $title, 'body' => $body]];
+        $title = 'dummy page';
+        $body = '<h1>Dummy</h1>';
+        $dummyContent = (new \Wizin\Bundle\SimpleCmsBundle\Entity\Content())
+            ->setPathInfo('/dummy')
+            ->setTitle($title)
+            ->setParameters(['body' => $body])
+            ->setTemplate('default.html.twig')
+        ;
+        $data[] = [$dummyContent, ['title' => $title, 'body' => $body]];
+
+        return $data;
+    }
+
+    /**
      * @return \Wizin\Bundle\SimpleCmsBundle\Service\Template
      */
     protected function getService()
@@ -58,4 +142,12 @@ class TemplateTest extends ServiceTestCase
         return $this->getContainer()->get('wizin_simple_cms.template');
     }
 
+    /**
+     * @param string $templateFile
+     * @return string path of test file
+     */
+    private function getTestFilePath($templateFile = 'default.html.twig')
+    {
+        return dirname(dirname(__DIR__)) . '/Resources/templates/' .$templateFile;
+    }
 }
