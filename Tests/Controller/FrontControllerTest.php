042a6e96b81eb22b91ae198837ed6041d24b2775
Makoto Hashiguchi <gusagi@gmail.com>
2015-05-24 02:37:18 +0900

    Add test for FrontController::showAction()

--- Tests/Controller/FrontControllerTest.php
+++ Tests/Controller/FrontControllerTest.php
@@ -2,8 +2,49 @@
 
 namespace Wizin\Bundle\SimpleCmsBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Wizin\Bundle\BaseBundle\TestCase\FunctionalTestCase;
+use Wizin\Bundle\SimpleCmsBundle\DataFixtures\ORM\ContentFixtureLoader;
 
-class FrontControllerTest extends WebTestCase
+class FrontControllerTest extends FunctionalTestCase
 {
+    /**
+     * @return null
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->loadFixtures(
+            [
+                new ContentFixtureLoader(),
+            ]
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider showActionProvider
+     * @param string $uri
+     * @param bool $isSuccessful
+     * @param string $status
+     * @param string $title
+     */
+    public function showAction($uri, $isSuccessful, $status, $title)
+    {
+        $crawler = $this->client->request('GET', $uri);
+        $this->assertEquals($isSuccessful, $this->client->getResponse()->isSuccessful());
+        $this->assertEquals($status, $this->client->getResponse()->getStatusCode());
+        if ($isSuccessful) {
+            $this->assertEquals($title, $crawler->filter('title')->text());
+        } else {
+        }
+    }
+
+    public function showActionProvider()
+    {
+        $data = [];
+        $data[] = ['/cms/front/page1', true, '200', 'Test Page 1'];
+        $data[] = ['/cms/front/not_found', false, '404', null];
+
+        return $data;
+    }
 }
