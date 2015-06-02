ee3bb13d2fc86a91894266672ec2d4bf0ecf893f
Makoto Hashiguchi <gusagi@gmail.com>
2015-06-03 01:03:14 +0900

    Create Content service

---
+++ Service/Content.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Content Service
+ */
+namespace Wizin\Bundle\SimpleCmsBundle\Service;
+
+use Wizin\Bundle\BaseBundle\Service\Service;
+
+/**
+ * Class Content
+ * @package Wizin\Bundle\SimpleCmsBundle\Service
+ */
+class Content extends Service
+{
+}
