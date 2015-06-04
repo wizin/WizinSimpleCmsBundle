d4dae1ff88bfec778f8629567e42878033a7e171
Makoto Hashiguchi <gusagi@gmail.com>
2015-06-05 01:57:48 +0900

    Create ContentConverter service

---
+++ Service/ContentConverter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ContentConverter
+ */
+namespace Wizin\Bundle\SimpleCmsBundle\Service;
+
+use Wizin\Bundle\BaseBundle\Service\Service;
+
+/**
+ * Class ContentConverter
+ * @package Wizin\Bundle\SimpleCmsBundle\Service
+ */
+class ContentConverter extends Service
+{
+}
+
