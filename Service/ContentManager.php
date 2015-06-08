c6846017f85c53e9ab1b8dca388b637d07f75d15
Makoto Hashiguchi <gusagi@gmail.com>
2015-06-09 01:30:30 +0900

    Add ContentManager::save()

--- Service/ContentManager.php
+++ Service/ContentManager.php
@@ -5,6 +5,8 @@
 namespace Wizin\Bundle\SimpleCmsBundle\Service;
 
 use Wizin\Bundle\BaseBundle\Service\Service;
+use Wizin\Bundle\SimpleCmsBundle\Entity\Content;
+use Wizin\Bundle\SimpleCmsBundle\Exception\DuplicateContentException;
 
 /**
  * Class ContentManager
@@ -12,5 +14,29 @@ use Wizin\Bundle\BaseBundle\Service\Service;
  */
 class ContentManager extends Service
 {
+    /**
+     * @param Content $content
+     * @return bool
+     */
+    public function save(Content $content)
+    {
+        // duplicate check
+        if ($this->getClassLoader()->getContentRepository()->isDuplicated($content)) {
+            throw new DuplicateContentException();
+        }
+        // persist entity
+        $this->getEntityManager()->persist($content);
+        $this->getEntityManager()->flush();
+
+        return true;
+    }
+
+    /**
+     * @return \Wizin\Bundle\SimpleCmsBundle\Service\ClassLoader
+     */
+    protected function getClassLoader()
+    {
+        return $this->container->get('wizin_simple_cms.class_loader');
+    }
 }
 
--- Tests/Service/ContentManagerTest.php
+++ Tests/Service/ContentManagerTest.php
@@ -2,15 +2,85 @@
 namespace Wizin\Bundle\SimpleCmsBundle\Tests\Service;
 
 use Wizin\Bundle\BaseBundle\TestCase\ServiceTestCase;
+use Wizin\Bundle\SimpleCmsBundle\Entity\Content;
 
 class ContentManagerTest extends ServiceTestCase
 {
+    /**
+     * @return null
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->truncate();
+        $this->getEntityManager()->clear();
+    }
+
     /**
      * @test
      */
     public function isValidService()
     {
         $this->assertInstanceOf('\Wizin\Bundle\SimpleCmsBundle\Service\ContentManager', $this->getService());
+    }
+
+    /**
+     * @test
+     * @dataProvider saveProvider
+     */
+    public function save(Content $content, $isDuplicated)
+    {
+        $repository = $this
+            ->getMockBuilder('\Wizin\Bundle\SimpleCmsBundle\Repository\ContentRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repository->expects($this->any())
+            ->method('isDuplicated')
+            ->will($this->returnValue($isDuplicated));
+        $loader = $this
+            ->getMockBuilder('\Wizin\Bundle\SimpleCmsBundle\Service\ClassLoader')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $loader->expects($this->any())
+            ->method('getContentRepository')
+            ->will($this->returnValue($repository));
+        $this->getContainer()->set('wizin_simple_cms.class_loader', $loader);
+        if ($isDuplicated) {
+            $this->setExpectedException('Wizin\Bundle\SimpleCmsBundle\Exception\DuplicateContentException');
+            $this->getService()->save($content);
+        } else {
+            $this->getService()->save($content);
+            $repository = $this->getEntityManager()->getRepository('WizinSimpleCmsBundle:Content');
+            $result = $repository->find($content->getId());
+            $this->assertInstanceOf('Wizin\Bundle\SimpleCmsBundle\Entity\Content', $result);
+            $this->assertNotNull($result->getId());
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function saveProvider()
+    {
+        $data = [];
+        $content = (new Content())
+            ->setPathInfo('/cms/front/new_page')
+            ->setTitle('new page')
+            ->setParameters(['body' => 'new page'])
+            ->setTemplateFile('default.html.twig')
+            ->setActive(true)
+        ;
+        $data[] = [$content, false];
+        $content = (new Content())
+            ->setPathInfo('/cms/front/page1')
+            ->setTitle('exist page')
+            ->setParameters(['body' => 'exist page'])
+            ->setTemplateFile('default.html.twig')
+            ->setActive(false)
+        ;
+        $data[] = [$content, true];
+
+        return $data;
     }
 
     /**
