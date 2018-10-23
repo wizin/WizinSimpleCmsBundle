d19b5bc3dd39c01944901bf736f5954533e31165
Makoto Hashiguchi <gusagi@gmail.com>
2018-10-23 16:00:43 +0900

    Use fully-qualified class name

--- Form/ContentType.php
+++ Form/ContentType.php
@@ -6,6 +6,7 @@ namespace Wizin\Bundle\SimpleCmsBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type;
 
 class ContentType extends AbstractType
 {
@@ -15,14 +16,14 @@ class ContentType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('id', 'hidden');
-        $builder->add('pathInfo');
-        $builder->add('title');
-        $builder->add('parameters', 'collection', ['type' => 'textarea', 'required' => false]);
-        $builder->add('templateFile', 'text', ['read_only' => true]);
-        $builder->add('active', 'checkbox', ['required' => false]);
-        $builder->add('save', 'submit', ['validation_groups' => false]);
-        $builder->add('draft', 'submit', ['validation_groups' => false]);
+        $builder->add('id', Type\HiddenType::class);
+        $builder->add('pathInfo', Type\TextType::class);
+        $builder->add('title', Type\TextType::class);
+        $builder->add('parameters', Type\CollectionType::class, ['type' => 'textarea', 'required' => false]);
+        $builder->add('templateFile', Type\TextType::class, ['read_only' => true]);
+        $builder->add('active', Type\CheckboxType::class, ['required' => false]);
+        $builder->add('save', Type\SubmitType::class, ['validation_groups' => false]);
+        $builder->add('draft', Type\SubmitType::class, ['validation_groups' => false]);
     }
 
     /**
